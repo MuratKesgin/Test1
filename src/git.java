a335f1e9ab1b8eabb9fa57d024f851dc6bc755b8
MuratKesgin <mrtgrhnksgn@gmail.com>
2020-08-09 01:10:20 -0400

    "addednew"

---
+++ src/git.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class git {
+
+    public static void main(String[] args) {
+        System.out.println("git short cut");
+    }
+}
