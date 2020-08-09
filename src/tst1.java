9fa4023f0f50f8ce5fb8ce0848e4c3aada0463da
MuratKesgin <mrtgrhnksgn@gmail.com>
2020-08-09 08:58:52 -0400

    asd

--- src/tst1.java
+++ src/tst1.java
@@ -1,7 +1,7 @@
 public class tst1 {
     public static void main(String[] args) {
         System.out.println("11232456");
-        System.out.println("11232456");
+        System.out.println("1123245612");
 
 
     }
